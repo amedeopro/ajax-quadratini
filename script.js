0c41f6df6e15e92a8266050d930eea29c0547acd
amedeopro <amedeopro@me.com>
2018-12-12 00:32:29 +0100

    ho messo due volte funzi. click e non va bene, numeri ok, colori no

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 $(document).ready(function(){
-
-
+$('.box').click(function(){
+for (var i = 0; i < 1; i++) {
     $.ajax({
       url: 'https://www.boolean.careers/api/random/int',
       method: 'GET',
       success: function(data){
-        $('.box').click(function(){
-          for (var i = 0; i < 36; i++) {
-            var numero = data.response
-            console.log(data.response)
+          var numero = data.response;
+          console.log(numero);
+          $('.box').click(function(){
+            var inserisciNumero = $(this).text(numero);
+
             if (numero <= 5) {
-              $(this).addClass('bg-giallo');
-            } else if (numero > 5) {
-              $(this).addClass('bg-verde');
-            }
-
-            var numeroInserito = $(this).text(numero);
-        }
+                $(this).addClass('bg-giallo');
+              } else if (numero > 5) {
+                $(this).addClass('bg-verde');
+              }
+          })
 
 
-       })
       },
       error: function(){
         alert('si è verificato un errore');
       }
     });
-
+}
 
 });
+})
