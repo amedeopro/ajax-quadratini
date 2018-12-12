1b035876fee467256e5758d34a8f2917c6605aef
amedeopro <amedeopro@me.com>
2018-12-12 10:09:31 +0100

    ok tutto funzionante

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
 $(document).ready(function(){
-$('.box').click(function(){
-for (var i = 0; i < 1; i++) {
-    $.ajax({
-      url: 'https://www.boolean.careers/api/random/int',
-      method: 'GET',
-      success: function(data){
-          var numero = data.response;
-          console.log(numero);
-          $('.box').click(function(){
-            var inserisciNumero = $(this).text(numero);
 
-            if (numero <= 5) {
-                $(this).addClass('bg-giallo');
-              } else if (numero > 5) {
-                $(this).addClass('bg-verde');
-              }
-          })
+  $('.box').click(function(){
+
+    var questoElemento = $(this)
+
+      $.ajax({
+        url: 'https://www.boolean.careers/api/random/int',
+        method: 'GET',
+        success: function(data){
+            var numero = data.response;
+            console.log(numero);
+
+            questoElemento.text(numero);
+
+              if (numero <= 5) {
+                  questoElemento.addClass('bg-giallo');
+                } else if (numero > 5) {
+                  questoElemento.addClass('bg-verde');
+                }
 
 
-      },
-      error: function(){
-        alert('si è verificato un errore');
-      }
-    });
-}
 
-});
-})
+        },
+        error: function(){
+          alert('si è verificato un errore');
+        }
+      });
+
+  });
+  })
